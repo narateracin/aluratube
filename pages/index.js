f0165d2c4963f6591bd9fdb16d2f6df70d8e15a0
Nara Teracin <nara.teracin@gmail.com>
2022-11-08 17:40:59 -0300

    acrescimo do search e do filtro da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
 
 function HomePage() {
     //const mensagem = "Bem-vindo ao AluraTube!";
-    const estiloDaHomePage = {backgroundColor: "red"};
+    const estiloDaHomePage = {display: "flex", flexDirection: "column", flex:1, /*backgroundColor: "red"*/};
+    //console.log(config.playlists);
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
-    console.log(config.playlists);
-    
     return (
         //<div style={estiloDaHomePage}>{mensagem}</div>
         <>
             <CSSReset/>
             <div style={estiloDaHomePage}>
-                <Menu />
+                <Menu valorDoFiltro= {valorDoFiltro} setValorDoFiltro= {setValorDoFiltro} />
                 <Header />
-                <Timeline playlists={config.playlists} />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists} />
             </div>
         </>
     )
@@ -47,10 +50,17 @@ function HomePage() {
     }
   `;
 
+  const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    height: 230px;
+  `;
+// background-image: url(${config.bg});
+
   function Header() {
     return (
         <StyledHeader>
-            <img src="banner"/>
+            <StyledBanner bg={config.bg} />
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`}/>
                 <div>
@@ -66,7 +76,7 @@ function HomePage() {
     )
   }
 
-  function Timeline(props) {
+  function Timeline({searchValue, ...props}) {
     //console.log("dentro do componente", props);
     const playlistNames = Object.keys(props.playlists);
     // Statement
@@ -75,22 +85,28 @@ function HomePage() {
         <StyledTimeline>
             {playlistNames.map((playlistName) => {
                 const videos = props.playlists[playlistName];
-                console.log(playlistName);
-                console.log(videos);
+                //console.log(playlistName);
+                //console.log(videos);
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
-                                    return(
-                                        <a href={video.url}>
-                                            <img src={video.thumb}/>
-                                            <span>
-                                                {video.title}
-                                            </span>
-                                        </a>
-                                    )
+                            {videos
+                                .filter((video) => {
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+                                    return titleNormalized.includes(searchValueNormalized)
                                 })
+                                .map((video) => {
+                                        return(
+                                            <a key={video.url} href={video.url}>
+                                                <img src={video.thumb}/>
+                                                <span>
+                                                    {video.title}
+                                                </span>
+                                            </a>
+                                        )
+                                    })
                             }
                         </div>
                     </section>
